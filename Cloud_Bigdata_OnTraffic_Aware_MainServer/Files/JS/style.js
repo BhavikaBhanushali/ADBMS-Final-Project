5d856fdacd32b17b2d01da4f6eb9f8b3c8cff7a5
alpeshahir31 <32401395+alpeshahir31@users.noreply.github.com>
2017-12-12 14:26:28 -0800

    Delete style.js

--- Cloud_Bigdata_OnTraffic_Aware_MainServer/Files/JS/style.js
+++
@@ -1,8 +0,0 @@
-function hideMessage()
-{
-	document.getElementById("message").style.display="none"; 
-}
-function startTimer() 
-{
-	var tim = window.setTimeout("hideMessage()", 5000);  // 5000 milliseconds = 5 seconds
-}
